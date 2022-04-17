5740a41913afe630540f7e85df6e6284d3693fcd
Patosha <Patosha68@gmail.com>
2022-04-17 14:46:48 +0300

    feat: lesson 11 layout + grid

--- src/App.tsx
+++ src/App.tsx
@@ -38,9 +38,36 @@ function App() {
                     </li>
                 </ul>
             </nav>
+
             <div className='content'>
-                Main content
+
+                <img className='contentImg'
+                     src="https://funart.pro/uploads/posts/2021-03/1617066508_28-p-oboi-kartinki-na-zastavku-kompyutera-30.jpg"
+                     alt="landscape"
+                />
+
+                <div>
+                    ava + description
+                </div>
+
+                <div>
+                    My posts
+
+                    <div>
+                        New post
+                    </div>
+
+                    <div>
+                        <div>
+                            post 1
+                        </div>
+                        <div>
+                            post 2
+                        </div>
+                    </div>
+                </div>
             </div>
+
         </div>
     );
 }
